462d5a49f5fa2c8215ccc12c135556b01d311801
Christian Stewart <christian@paral.in>
2019-02-15 18:19:41 -0800

    virtcontainers: add qemu arm definition file
    Adds support for the "arm" build architecture.
    
    This is experimental.
    
    Signed-off-by: Christian Stewart <christian@paral.in>

---
+++ arch/arm-options.mk
@@ -0,0 +1,12 @@
+# Copyright (c) 2018 Intel Corporation
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# ARM 64 settings
+
+MACHINETYPE := virt
+KERNELPARAMS :=
+MACHINEACCELERATORS :=
+
+QEMUCMD := qemu-system-arm
